52165ff6974e48a3c3e0608c97c92288ed884ba1
petemeng <570696628@qq.com>
2025-11-21 14:57:26 +0800

    Quartz sync: Nov 21, 2025, 2:57 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "petemeng.github.io/Notes_for_Peter",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
